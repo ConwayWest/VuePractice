34fcad41014c15c8ff08786d7222590c3dca3703
Kyle Conway <kyleconway@rocketmail.com>
2020-10-22 18:03:18 -0400

    Sync 10222020

---
+++ section_8/practice/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <form @submit.prevent="submitUser">
+      <div>
+        <label>Name</label>
+        <input type="text" v-model="enteredName" />
+      </div>
+      <div>
+        <label>Age</label>
+        <input type="text" v-model="enteredAge" />
+      </div>
+      <button>Set User</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ['set-user'],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    submitUser() {
+      this.$emit(
+        "set-user",
+        this.enteredName,
+        this.enteredAge
+      );
+      this.enteredName = '';
+      this.enteredAge = '';
+    },
+  }
+};
+</script>
