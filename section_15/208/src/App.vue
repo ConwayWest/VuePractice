8476db3c24bd7562ea75988dffdd551d694ac8ae
Kyle Conway <kyleconway@rocketmail.com>
2020-12-07 09:35:57 -0500

    Sync 12062020

--- section_15/208/src/App.vue
+++ section_15/208/src/App.vue
@@ -1,13 +1,50 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex" v-if="userIsAuthenticated">
+    <the-counter></the-counter>
+    <favorite-value></favorite-value>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+  <base-container title="Auth">
+    <user-auth>
+    </user-auth>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
+import { mapGetters } from 'vuex';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue,
+    UserAuth
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+    ...mapGetters(['userIsAuthenticated'])
+  },
+  methods: {
+    addOne() {
+      // this.$store.commit('increase', { value: 10 });
+      // this.$store.commit({
+      //   type: 'increase',
+      //   value: 10
+      // });
+      this.$store.dispatch({
+        type: 'numbers/increase',
+        value: 10,
+      });
+    },
   },
 };
 </script>
---
+++ section_15/208/src/components/UserAuth.vue
@@ -0,0 +1,19 @@
+<template>
+  <button v-if="!userIsAuthenticated" @click="login">Login</button>
+  <button v-if="userIsAuthenticated" @click="logout">Logout</button>
+  <p>{{ testAuth }}</p>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  methods: {
+    ...mapActions(['login', 'logout'])
+  },
+  computed: {
+    ...mapGetters(['userIsAuthenticated']),
+    ...mapGetters('numbers', ['testAuth'])
+  },
+}
+</script>
